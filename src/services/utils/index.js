33df74bc23f321adeeb6c5d46fc70940b8ba5bcb
peace-jung <hopuo132@gmail.com>
2019-12-15 23:17:25 +0900

    url 수정

--- src/services/utils/index.js
+++ src/services/utils/index.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:3001/instagram';
-// export const baseUrl = 'https://mingki-talk.herokuapp.com/instagram';
+// export const baseUrl = 'http://localhost:3001/instagram';
+export const baseUrl = 'https://mingki-talk.herokuapp.com/instagram';
